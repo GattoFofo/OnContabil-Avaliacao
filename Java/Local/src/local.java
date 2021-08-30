9715375d27c71987648e18c2e54c95e6b16d76cf
Gustavo Canicali Peruchi <gu12peruchi@hotmai.com>
2021-08-30 00:34:11 -0300

    New comeco de enviar arquivos

--- Java/Local/src/local.java
+++ Java/Local/src/local.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -25,6 +28,24 @@ public class local {
                     .filter(Files::isRegularFile)
                     .filter(path -> path.getFileName().toString().endsWith("xml"))
                     .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*Manda arquivos para um endereco web*/
+        String url = "http://localhost:8000";
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-type", "text/xml");
+            connection.setDoOutput(true);
+
+            DataOutputStream request = new DataOutputStream(connection.getOutputStream());
+            request.writeBytes("test\r\n");
+            request.flush();
+            request.close();
+
+            int responseCode = connection.getResponseCode();
+            System.out.println(responseCode); // Should be 200
         } catch (IOException e) {
             e.printStackTrace();
         }
